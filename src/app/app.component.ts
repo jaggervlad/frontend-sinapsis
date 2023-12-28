139157991777992c2be628baa12121a10d2672b8
Sebastian Acosta Alonso <45018754+jaggervlad@users.noreply.github.com>
2023-12-28 01:56:41 -0500

    chore: :tada: setup tailwindcss, create al require logic to show message report

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,66 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-test';
+  messagesReport: any[] = [];
+  clients: any[] = [];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.http
+      .get(`http://localhost:3000/dev/api/clients`)
+      .subscribe((clientRes: any) => {
+        this.clients = clientRes.data;
+      });
+
+    this.route.queryParams.subscribe((params) => {
+      if (params['month']) {
+        this.fetchMessageReport(params['month'], params['clientId']);
+      }
+    });
+  }
+
+  onChangeMonth(event: any) {
+    const month = event.target.value.split('-')[1];
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { month },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  onChangeClient(event: any) {
+    const clientId = event.target.value;
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { clientId },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  fetchMessageReport(month: number, clientId?: number) {
+    this.http
+      .get(
+        `http://localhost:3000/dev/api/messages/report?month=${month}&clientId=${clientId}`
+      )
+      .subscribe((report: any) => {
+        this.messagesReport = report.data;
+      });
+  }
 }
